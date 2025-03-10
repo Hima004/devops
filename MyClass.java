816df02f10707b46b48c0a204eed724f10447cf1
Hima004 <113834955+Hima004@users.noreply.github.com>
2025-03-10 14:33:59 +0530

    Update MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -2,21 +2,14 @@ import java.util.Scanner;
 
 public class MyClass {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Prompt the user for two numbers
-        System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
-
-        System.out.print("Enter the second number: ");
-        int num2 = scanner.nextInt();
+        
+        int num1 = 2;
+        int num2 = 4;
 
         // Add the numbers
         int sum = num1 + num2;
 
         // Display the result
         System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
-
-        scanner.close();
     }
 }
