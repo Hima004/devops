3d61baa048fe97ee69746e33d9f1304a28ec18c0
Hima004 <113834955+Hima004@users.noreply.github.com>
2025-03-10 14:56:25 +0530

    Create TwoNumbers.java

---
+++ TwoNumbers.java
@@ -0,0 +1,13 @@
+public class TwoNumbers {
+    public static void main(String[] args) {
+        // Directly assign values to the numbers
+        int num1 = 10;
+        int num2 = 20;
+
+        // Add the numbers
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
